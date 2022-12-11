76638005ad7e79568eb49affcc72bb0529877f05
ttodoshi <sheshin.alexanderkpc2052020@gmail.com>
2022-12-11 20:36:28 +0300

    экспорт excel

--- passenger-transportation/MainWindow.xaml.cs
+++ passenger-transportation/MainWindow.xaml.cs
@@ -6,6 +6,8 @@ using System.Windows.Controls;
 using Newtonsoft.Json;
 using System.Text.Json.Nodes;
 using System.Collections.Generic;
+using OfficeOpenXml;
+using System.IO.Packaging;
 
 namespace passenger_transportation
 {
@@ -130,9 +132,13 @@ namespace passenger_transportation
                 staff.Add(obj);
             }
             var json = JsonConvert.SerializeObject(staff, Formatting.Indented);
+
             if (InputWindow.ShowDialog() == true)
             {
-                File.WriteAllText(InputWindow.pathInput.Text, json);
+                string strPath = InputWindow.pathInput.Text;
+                if (File.Exists(strPath))
+                    File.Delete(strPath);
+                File.WriteAllText(strPath, json);
             }
         }
         public class JsonStaff
@@ -143,7 +149,43 @@ namespace passenger_transportation
         // Экспорт в xlsx
         private void Export_Xlsx_Click(object sender, RoutedEventArgs e)
         {
+            InputWindow InputWindow = new InputWindow();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            ExcelPackage package = new ExcelPackage();
+            ExcelWorksheet sheet = package.Workbook.Worksheets.Add("Лист1");
+            string[] headers = { "Id", "Короткий Id", "Фамилия", "Имя", "Отчество", "Дата рождения", "Телефон", "Отдел" };
+            for (int i = 1; i <= headers.Length; i++)
+            {
+                sheet.Cells[1, i].Value = headers[i-1];
+            }
+            int currString = 2;
+            foreach (var person in db.Staff.Local.ToObservableCollection())
+            {
+                sheet.Cells[currString, 1].Value = person.Id;
+                sheet.Cells[currString, 2].Value = person.ShortId;
+                sheet.Cells[currString, 3].Value = person.LastName;
+                sheet.Cells[currString, 4].Value = person.Name;
+                sheet.Cells[currString, 5].Value = person.Patronymic;
+                sheet.Cells[currString, 6].Value = person.BirthdayDate;
+                sheet.Cells[currString, 7].Value = person.ContactPhone;
+                sheet.Cells[currString, 8].Value = person.Department;
+                currString += 1;
+            }
+            for (int i = 1; i <= headers.Length; i++)
+            {
+                sheet.Column(i).AutoFit();
+            }
+            if (InputWindow.ShowDialog() == true)
+            {
+                string strPath = InputWindow.pathInput.Text;
+                if (File.Exists(strPath))
+                    File.Delete(strPath);
+                FileStream objFileStrm = File.Create(strPath);
+                objFileStrm.Close();
+                File.WriteAllBytes(strPath, package.GetAsByteArray());
 
+            }
+            package.Dispose();
         }
     }
 }
